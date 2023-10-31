06c89178e93bfd10228afda86b5f8d6deaef3b3e
MDIMACat <romanmdima@gmail.com>
2023-10-31 15:33:07 +0200

    changed .h file

--- binary_trees.h
+++ binary_trees.h
@@ -43,42 +43,12 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second);
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-int binary_tree_is_complete(const binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-int binary_tree_is_bst(const binary_tree_t *tree);
-bst_t *bst_insert(bst_t **tree, int value);
-bst_t *array_to_bst(int *array, size_t size);
-bst_t *bst_search(const bst_t *tree, int value);
-bst_t *bst_remove(bst_t *root, int value);
-int binary_tree_is_avl(const binary_tree_t *tree);
-avl_t *avl_insert(avl_t **tree, int value);
-avl_t *array_to_avl(int *array, size_t size);
-avl_t *avl_remove(avl_t *root, int value);
-avl_t *sorted_array_to_avl(int *array, size_t size);
-int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value);
-heap_t *array_to_heap(int *array, size_t size);
-int heap_extract(heap_t **root);
-int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+/*HELPER FUNCTIONS*/
+int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 
-
-/* Helper functions */
-int binary_subtree_height(const binary_tree_t *subtree);
-int binary_subtree_balance(const binary_tree_t *subtree);
-int binary_subtree_depth(const binary_tree_t *subtree);
-int is_direct_descendant(const binary_tree_t *node,
-		const binary_tree_t *assumed_ancestor);
-binary_tree_t *find_immediate_ancestor
-	(const binary_tree_t *first,
-	const binary_tree_t *second,
-	int first_node_depth,
-	int second_node_depth);
-void print_nodes_at(int level, const binary_tree_t *subtree,
-		void (*func)(int));
 
 #endif
