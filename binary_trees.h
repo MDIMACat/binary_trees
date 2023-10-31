c3bd53d88e713588c31217bbe1e7771897275ef3
MDIMACat <romanmdima@gmail.com>
2023-10-31 15:35:18 +0200

    Changed h file

--- binary_trees.h
+++ binary_trees.h
@@ -44,11 +44,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-/*HELPER FUNCTIONS*/
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-size_t _height(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
-
-
-
 #endif
